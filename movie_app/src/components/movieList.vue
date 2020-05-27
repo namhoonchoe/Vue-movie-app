ac5611b22f3042afdef2dd75892150ffdc24c6c7
namhoonchoe <33464899+namhoonchoe@users.noreply.github.com>
2020-05-27 15:58:45 +0900

    Movie component #1
    movie list rendering #1

---
+++ movie_app/src/components/movieList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class ="movieCard">
+      <p>{{title}}</p>
+      <img :src=$props.medium_cover_image alt="">
+      <p>{{id}}</p>
+      <p>{{summary}}</p>
+      <p>{{year}}</p>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name:'Movie',
+    props:{
+        id:String,
+        year:Number,
+        title:String,
+        summary:String,
+        medium_cover_image:String
+    }
+};
+</script>
+
+<style>
+
+</style>
